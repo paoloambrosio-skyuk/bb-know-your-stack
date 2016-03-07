547c84fc3e3aa1dfabfb2e604885f5bad8b2b27e
Paolo Ambrosio <paolo.ambrosio2@sky.uk>
2016-03-07 17:15:35 +0000

    Using graphite-web instead of graphite-api that DOES NOT WORK

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -13,13 +13,17 @@ dpkg -i /tmp/saboteur_0.7_all.deb
 update-rc.d saboteur-agent defaults 95 10
 service saboteur-agent start
 
-apt-get install -y graphite-carbon
+apt-get install -y graphite-carbon graphite-web apache2 libapache2-mod-wsgi
 cp /usr/share/doc/graphite-carbon/examples/storage-aggregation.conf.example /etc/carbon/storage-aggregation.conf
 echo 'CARBON_CACHE_ENABLED=true' >/etc/default/graphite-carbon
+graphite-build-search-index
 service carbon-cache start
 
-wget -nv -P /tmp https://github.com/brutasse/graphite-api/releases/download/1.1.2/graphite-api_1.1.2-1447943657-ubuntu14.04_amd64.deb
-dpkg -i /tmp/graphite-api_1.1.2-1447943657-ubuntu14.04_amd64.deb
+echo no | sudo -u _graphite graphite-manage syncdb
+ln -s /usr/share/graphite-web/apache2-graphite.conf /etc/apache2/sites-available/graphite-web.conf
+a2dissite *
+a2ensite graphite-web
+service apache2 reload
 
 wget -nv -P /tmp https://grafanarel.s3.amazonaws.com/builds/grafana_2.6.0_amd64.deb
 apt-get install -y adduser libfontconfig
@@ -32,4 +36,4 @@ do
   echo 'Waiting for Grafana'
   sleep 3
 done
-curl -s -H 'Content-Type: application/json' -X POST 'http://admin:admin@localhost:3000/api/datasources' --data-binary '{"name":"test","type":"graphite","url":"http://localhost:8888","access":"proxy","basicAuth":false,"isDefault":true}'
+curl -s -H 'Content-Type: application/json' -X POST 'http://admin:admin@localhost:3000/api/datasources' --data-binary '{"name":"test","type":"graphite","url":"http://localhost","access":"proxy","basicAuth":false,"isDefault":true}'
